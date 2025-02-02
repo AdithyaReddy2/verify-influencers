1666d668277208c991d176de357228e9765b13bd
AdithyaReddy2 <palagirisaiadithyareddy@gmail.com>
2025-02-02 17:12:04 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/login", { email, password });
+      const res = await axios.post("/api/login", { email, password });
       localStorage.setItem("userId", res.data.userId);
       localStorage.setItem("username", res.data.email);
       navigate("/leaderboard");
