1c6ec5cc39025a5ea6144925d94fbf8939b845b0
AdithyaReddy2 <palagirisaiadithyareddy@gmail.com>
2025-02-02 17:15:12 +0530

    Update Leaderboard.jsx

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -9,7 +9,7 @@ export default function Leaderboard() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/influencers")
+    axios.get("/api/influencers")
       .then((res) => {
         console.log("Fetched influencers:", res.data);
         setInfluencers(res.data);
